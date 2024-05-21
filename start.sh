bab5a8e637d89a383238f5ea924e8c11e791a0cc
Nidelson Gimenez <nidelson@gmail.com>
2024-05-20 21:11:26 -0300

    chore: history commands

---
+++ start.sh
@@ -0,0 +1,9 @@
+yarn add axios
+
+yarn add @react-navigation/native
+yarn add @react-navigation/native-stack
+
+expo install react-native-screens   
+expo install react-native-safe-area-context
+
+expo install expo-secure-store
