dfbbdc06e5dd20786675e38e803c3a4c93960258
Nidelson Gimenez <nidelson@gmail.com>
2024-05-20 21:02:18 -0300

    feat: implements userService

--- app/services/userService.ts
+++ app/services/userService.ts
@@ -1,17 +1,10 @@
 import axios from 'axios';
 
-const TOKEN_KEY = 'token';
-export const API_URL = 'https://api.developerbetterapps.com';
+const API_URL = 'https://api.developbetterapps.com';
 
 export const register = async (email: string, password: string) => {
-  try {
-    return await axios.post(`${API_URL}/users`, {
-      email,
-      password,
-    });
-  } catch (error) {
-    console.log(error);
-
-    return { error: true, message: (error as any).response.data.message };
-  }
+  return await axios.post(`${API_URL}/users`, {
+    email,
+    password,
+  });
 };
