3756a9c42e3c72e2c605e44d1f3c6cf96fb07bed
Nidelson Gimenez <nidelson@gmail.com>
2024-05-20 21:06:14 -0300

    feat: apply style

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native';
 import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 const Home = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Home</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default Home;
